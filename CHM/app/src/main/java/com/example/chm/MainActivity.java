6fbe590815d3ceb45739c35b837ee35793172999
seungjae6874 <qkrtmdwo1@gmail.com>
2020-05-18 02:17:01 +0900

    산학 어플 ver1.0

---
+++ CHM/app/src/main/java/com/example/chm/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.chm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        final EditText period = findViewById(R.id.기간수);
+        Spinner spinner = findViewById(R.id.period);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}
