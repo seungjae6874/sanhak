601dea8309dd901ef0c8943376ea2be095ff8e50
heejae369 <51227242+heejae369@users.noreply.github.com>
2020-06-15 15:00:40 +0900

    AutoCompleteTextView Add

--- AndroidEventGenerator/app/src/main/java/com/example/androideventgenerator/MyInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.androideventgenerator;
-
-import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunction;
-
-public interface MyInterface {
-
-    @LambdaFunction
-    ResponseClass diet_rd_n_cal(RequestClass request);
-}
